a637b8a62aa5ed4851326116aa567108a3326b39
milan978gh <milan978@gmail.com>
2020-08-30 22:47:18 +0200

    js started

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+// event listeners
+todoButton.addEventListener("click", addTodo);
+
+// functions
+function addTodo(event) {
+    // prevent form from submitting
+    event.preventDefault();
+    // todo div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    // create li
+    const newTodo = document.createElement("li");
+    newTodo.innerText = hey;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+}
