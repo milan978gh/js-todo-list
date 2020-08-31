52dc5d57370150e6d939bca03ebf82c7709dbc99
milan978gh <milan978@gmail.com>
2020-08-31 23:23:48 +0200

    work in progres

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ const todoList = document.querySelector(".todo-list");
 
 // event listeners
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+
 
 // functions
 function addTodo(event) {
@@ -15,7 +17,39 @@ function addTodo(event) {
     todoDiv.classList.add("todo");
     // create li
     const newTodo = document.createElement("li");
-    newTodo.innerText = hey;
+    newTodo.innerText = todoInput.value;
     newTodo.classList.add("todo-item");
     todoDiv.appendChild(newTodo);
+    // check-mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    // trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    // append to list
+    todoList.appendChild(todoDiv);
+    // clear todoInput value
+    todoInput.value = "";
+}
+
+function deleteCheck(e) {
+    const item = e.target;
+    // delete todo
+    if (item.classList[0] === "trash-btn") {
+        const todo = item.parentElement;
+        // animation
+        todo.classList.add("fall");
+        todo.addEventListener("transitionend", function () {
+            todo.remove();
+        });
+    }
+    // check-mark todo
+    if (item.classList[0] === "complete-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
 }
